0ef9b8084964f79de4a65831decc4c07af345aac
Kamrul Hasan <38207177+kamrul9670@users.noreply.github.com>
2024-12-08 02:27:19 +0530

    Update api.js
    update api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "button-store-lzy3.vercel.app", // Replace with your API base URL
+  baseURL: "https://button-store-tmeo.vercel.app/", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
   },
