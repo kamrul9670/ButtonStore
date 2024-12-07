20a912fc4f11f9d519315e2db2f78501ca98ccf3
kamrul9670 <kamrul9670@gmail.com>
2024-12-08 03:11:58 +0530

    firsttt

---
+++ server/api/auth.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('../Routes/AuthRouter');
+require('dotenv').config();
+require('../Models/db');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors({
+  origin: ['https://button-store-verg.vercel.app/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json("Successfully deployed Server");
+});
+
+app.use('/api/auth', AuthRouter);
+
+// Vercel serverless function
+module.exports = (req, res) => {
+  app(req, res);
+};
